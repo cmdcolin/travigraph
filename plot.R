8e7c9f00abe2dad55a8dbd7c048ba71bec25729a
Colin <colin.diesh@gmail.com>
2015-09-29 22:01:07 -0500

    Update seconds to minutes

--- plot.R
+++ plot.R
@@ -18,6 +18,7 @@ results=results[complete.cases(results),]
 outfile=args[2]
 width=as.numeric(args[3])
 height=as.numeric(args[4])
+repo=args[5]
 
 if(is.na(width))width=1000
 if(is.na(height))height=600
@@ -25,16 +26,16 @@ if(is.na(height))height=600
 # lubridate and calcualte duration from started_at-finished_at
 results[,2]=ymd_hms(results[,2])
 results[,3]=ymd_hms(results[,3])
-duration=as.numeric(seconds(results[,3]-results[,2]))
+duration=as.numeric(seconds(results[,3]-results[,2]))/60
 results=cbind(results,duration)
 colnames(results)<-c('state','started_at','finished_at','duration')
 
 myplot<-ggplot(results,aes(started_at,duration), colour=state) +
     geom_point(aes(colour=state))+
-    ggtitle("Travis-CI Builds") +
+    ggtitle(paste(repo,"builds")) +
     xlab("Date") +
     ylab("Build time (minutes)") +
-    scale_colour_brewer(type="seq", palette="Spectral")
+    scale_colour_brewer(type="seq", palette="Spectral") 
 
 png(outfile,width=width,height=height)
 myplot
